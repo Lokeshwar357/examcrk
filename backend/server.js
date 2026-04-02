bdde76f3b734c85976839f8a0df4292b69d56df2
Lokeshwar357 <lokeshreddybanka@gmail.com>
2026-04-02 15:42:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: [process.env.FRONTEND_URL || "http://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
